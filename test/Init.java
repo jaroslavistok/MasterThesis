551ed2c2eeecf29dce8e74dba042adf4891e8234
Jaroslav Ištok <jaroslav.istok@gmail.com>
2018-02-01 11:06:29 +0100

    Initialized project and added basic features

---
+++ test/Init.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Init {
+
+
+    @Test
+    public void passingTest(){
+        assertEquals(1, 1);
+
+
+    }
+}
